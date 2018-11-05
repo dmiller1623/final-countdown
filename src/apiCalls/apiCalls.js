ec2ce1636ea39273c54945b643dbea6aaa7eed38
Dennis <dmiller1623@gmail.com>
2018-11-05 15:12:12 -0700

    Add filter for the api call

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,7 +1,14 @@
 import apiKey from '../apiKey.js'
 
 export const getGalleries = async () => {
-  const response = await fetch(`https://api.harvardartmuseums.org/gallery?apikey=${apiKey}`)
+  let images = []
+  const response = await fetch(`https://api.harvardartmuseums.org/object?person=28241&apikey=${apiKey}&size=20`)
   const galleries = await response.json();
-  console.log(galleries)
+  let slicedGalleries = galleries.records.slice(4, 14)
+  slicedGalleries.forEach(resource => {
+    resource.images.forEach(image => {
+      images.push(image.baseimageurl)
+    })
+  })
+  return images
 }
