56f871fbff78fe790fd523824de6374f01d2e120
Dennis <dmiller1623@gmail.com>
2018-11-06 12:16:18 -0700

    Add snapshot testing for image card

--- src/tests/unit/ImageCard.spec.js
+++ src/tests/unit/ImageCard.spec.js
@@ -4,11 +4,7 @@ import ImageCard from '../../components/ImageCard';
 describe('ImageCard', () => {
   it('should do this', () => {
     const image = "https://nrs.harvard.edu/urn-3:HUAM:INV038751_dynmc"
-    const wrapper = shallowMount(ImageCard, {
-      propsData: {
-        image: image
-      }
-    })
+    const wrapper = shallowMount(ImageCard, {image: image})
     expect(wrapper).toMatchSnapshot()
   })
 })
